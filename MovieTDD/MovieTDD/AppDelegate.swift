176f6fc38894b012d7294ebd0f492adba0f113ea
kimdongjune <jun36817@naver.com>
2022-05-27 18:37:32 +0900

    [Init] project 초기설정 및 gitignore추가.

---
+++ MovieTDD/MovieTDD/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  MovieTDD
+//
+//  Created by 김동준 on 2022/05/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MovieTDD/MovieTDD/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieTDD
+//
+//  Created by 김동준 on 2022/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
